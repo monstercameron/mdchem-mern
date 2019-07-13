522268d80fe17a248f9660d9f6638c08c4fd659a
earl cameron <mr.e.cmaeron@gmail.com>
2019-07-13 17:06:40 -0400

    highscore crom job only runs on students that have data

--- services/services/Highscore.js
+++ services/services/Highscore.js
@@ -13,9 +13,11 @@ module.exports = class HighScoreUpdate {
             students.forEach(student => {
                 //console.log(student)
                 let score = 0
-                for (let key of Object.keys(student.data)) {
-                    //console.log(student.data[key].score)
-                    score += student.data[key].score
+                if(student.data){
+                    for (let key of Object.keys(student.data)) {
+                        //console.log(student.data[key].score)
+                        score += student.data[key].score
+                    }
                 }
                 student.updateOne({ score: score }, () => {
                     //console.log('updated!')
