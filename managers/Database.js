ddf87389829f18a7cd27816baa97604082635d2a
earl cameron <mr.e.cmaeron@gmail.com>
2019-07-23 03:31:31 -0400

    init testing config

--- managers/Database.js
+++ managers/Database.js
@@ -3,10 +3,12 @@
  */
 console.log(process.env.port)
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGO_LOCAL_CONN, {useNewUrlParser: true})
+mongoose.connect(process.env.MONGO_LOCAL_CONN, {
+  useNewUrlParser: true
+})
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', function () {
   console.log(`Connected to database ${process.env.MONGO_LOCAL_CONN}`)
 });
 module.exports = db
