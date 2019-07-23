4093f258acfa141767032893d56f9944bd76e7f4
earl cameron <mr.e.cmaeron@gmail.com>
2019-07-23 03:41:00 -0400

    jest 1st test case

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,11 +1,32 @@
 const request = require('supertest');
 const app = require('../app')
 
-describe('Test the root path', () => {
+describe('Testing Admin API', () => {
   test('It should response the GET method', (done) => {
-    request(app).get('/').then((response) => {
-      expect(response.statusCode).toBe(200);
-      done();
-    });
-  });
-});
+    request(app).get('/api/admin').then((response) => {
+      expect(response.statusCode).toBe(200)
+      done()
+    })
+  })
+  test('It should response the POST method', (done) => {
+    request(app).post('/api/admin').then((response) => {
+      expect(response.statusCode).toBe(200)
+      done()
+    })
+  })
+})
+
+describe('Testing Admin API', () => {
+  test('It should response the GET method', (done) => {
+    request(app).get('/api/admin').then((response) => {
+      expect(response.statusCode).toBe(200)
+      done()
+    })
+  })
+  test('It should response the POST method', (done) => {
+    request(app).post('/api/admin').then((response) => {
+      expect(response.statusCode).toBe(200)
+      done()
+    })
+  })
+})
