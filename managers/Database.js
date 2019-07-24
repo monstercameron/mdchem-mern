61463f88d566c13fd9bba2817024ceeeb0020632
earl cameron <mr.e.cmaeron@gmail.com>
2019-07-24 18:55:33 -0400

    clean up node env and collection ternery

--- managers/Database.js
+++ managers/Database.js
@@ -2,12 +2,13 @@
  * Database Manager
  */
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGO_TEST_CONN, {
+const DBURL = process.env.NODE_ENV === 'development' ? process.env.MONGO_TEST_CONN : process.env.MONGO_LOCAL_CONN
+mongoose.connect(DBURL, {
   useNewUrlParser: true
 })
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-  console.log(`Connected to database ${process.env.MONGO_TEST_CONN}`)
+  console.log(`Connected to database ${DBURL}`)
 });
 module.exports = db
