32154144250641eccc7e9a28ba699cd382f871e2
monstercameron <mr.e.cameron@gmail.com>
2019-08-04 21:22:48 -0400

    gd hashes

--- managers/Encrypt.js
+++ managers/Encrypt.js
@@ -7,7 +7,8 @@ class Hash {
         this.getHash(passwordText, callback)
     }
     getHash = (password, callback) => {
-        bcrypt.genSalt(parseInt(process.env.TOKEN_SALT_ROUNDS), function (err, salt) {
+        console.log(parseInt(process.env.TOKEN_SALT_ROUNDS))
+        bcrypt.genSalt(10, function (err, salt) {
             bcrypt.hash(password, salt, function (err, hash) {
                 // Store hash in your password DB.
                 console.log(hash)
